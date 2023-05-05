0376cf239a0a3c0ddc427e97c486c8111e5160dc
prashant7650 <prashantkad240999@gmail.com>
2023-05-05 20:58:14 +0530

    com

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ const menuBtn = document.querySelector('.hamburger');
 const menuBar = document.querySelector('.menu-bar');
 const menuList = document.querySelector('.nav-menu');
 
+
+
 menuBtn.addEventListener('click', showMenu);
+
+
 
 function showMenu(){
     menuBtn.classList.toggle('is-active');
