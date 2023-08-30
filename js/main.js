4ece856cb24bb9ee1bdfd4f1726b841c4d4bc121
prashant7650 <prashantkad240999@gmail.com>
2023-08-30 14:29:43 +0530

    ok

--- js/main.js
+++ js/main.js
@@ -1,94 +1,18 @@
-// const menuBtn = document.querySelector('.hamburger');
-// const menuBar = document.querySelector('.menu-bar');
-// const menuList = document.querySelector('.nav-menu');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('#nav-menu');
+const menuList = document.querySelector('.nav-menu');
 
-// menuBtn.addEventListener('click', showMenu);
+menuBtn.addEventListener('click', showMenu);
 
-// function showMenu(){
-//     menuBtn.classList.toggle('is-active');
-//     menuBar.classList.toggle('is-active');
-//     menuList.classList.toggle('is-active');
-// }
-
-
-
-
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('#nav-menu')
-
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active')
-};
+function showMenu(){
+    menuBtn.classList.toggle('is-active');
+    menuBar.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+}
 
 
 
 
-// *=============================== resume open in new tab ==================================
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('#nav-menu')
 
-
-    let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
-    let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
-
-    function NewTab() {
-      window.open(
-        "https://drive.google.com/drive/folders/1t58j0rDGz38tB-TYooWYN79laSym_kyx?usp=share_link",
-        "_blank"
-      );
-    }
-
-// *=============================== scroll sections active link ==================================
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a')
-
-window.onscroll = () => {
-  sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
-
-    if(top >= offset && top < offset + height) {
-        navlinks.forEach(links => {
-          links.classList.remove('active');
-          document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-        });
-    };
-
-  });
-  // *=============================== sticky navbar ==================================
-   
-  let header = document.querySelector('header');
-
-  header.classList.toggle('sticky', window.scrollY > 100);
-
-// *=============================== remove toggle icon and navbar when click navbar link(scroll) ==================================
-
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active')
-
-};
-
-// *=============================== scroll reveal ==================================
-
-ScrollReveal({
-  // reset: true,
-  distance: '80px',
-  duration: 2000,
-  delay: 200
-});
-
-ScrollReveal().reveal('.home-content, .heading', { origin:'top'});
-ScrollReveal().reveal('.home-img, .skills-container, .project-card, .contact form', { origin:'bottom'});
-ScrollReveal().reveal('.home-content h1', { origin:'left'});
-ScrollReveal().reveal('.home-content p, .about-content', { origin:'left'});
-
-// *===============================  typed js==================================
-
-const typed = new Typed('.multiple-text', {
-  strings: ['Full Stack Web Developer', 'Backend Developer', 'Frontend Developer'],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backdelay: 1000,
-  loop: true
-})
